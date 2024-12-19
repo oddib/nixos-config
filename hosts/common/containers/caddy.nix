a0dfea1b6bcb3ebcb30460bd8baf86862aa6c5a4
Oddbjørn Rønnestad <oddbjorn.r@gmail.com>
2024-12-19 20:50:24 +0100

    caddy reverse proxy

--- hosts/common/containers/caddy.nix
+++ hosts/common/containers/caddy.nix
@@ -1,16 +1,44 @@
-{ lib, config, users, ... }: {
+{ config, ... }: {
   virtualisation.oci-containers.containers.caddy = {
     image = "ghcr.io/caddybuilds/caddy-cloudflare:latest";
-    #environmentFiles = [config.opnix.secrets.caddy-secret.path];
+    environmentFiles = [config.opnix.secrets.caddy-secret.path];
+    environment = {
+      DOMAIN = "scuffedflix.no";};
     extraOptions = [ "--network=host" ];
-    
+    user= "root";
+    volumes = [
+      "/etc/container/caddy/Caddyfile:/etc/caddy/Caddyfile"
+      "caddy_data:/data"
+      "caddy_config:/config"
+    ];
+    #capabilities = {
+    #  NET_ADMIN = true;
+    #};
   };
-  users.users.caddy = {
-    name = "caddy";
-    extraGroups = [ "networkmanager" ];
-    isSystemUser = true;
-    group = "caddy";
+  environment.etc = {
+    caddy = {
+      #source = ./Caddyfile;
+      target = "/container/caddy/Caddyfile";
+      text ="
+(cloudflare) {
+	tls {
+		dns cloudflare {$CLOUDFLARE_API_KEY}
+	}
+}
+{$DOMAIN} {
+	reverse_proxy localhost:8096
+	import cloudflare
+}
+*.{$DOMAIN} {
+	import cloudflare
+    @test host test.{$DOMAIN}
 
+    @jellyseerr host jellyseerr.{$DOMAIN}, jellyserr.{$DOMAIN}, jellyseer.{$DOMAIN}, jellyser.{$DOMAIN}, request.{$DOMAIN} 
+    handle @jellyseerr {
+        reverse_proxy localhost:5055
+    }
+}
+      ";
+    };
   };
-  users.groups.caddy = {};
 }
--- hosts/common/containers/opnix.nix
+++ hosts/common/containers/opnix.nix
@@ -9,7 +9,7 @@
     environmentFile = "/etc/opnix.env";
     # Set the systemd services that will use 1Password secrets; this makes them wait until
     # secrets are deployed before attempting to start the service.
-    systemdWantedBy = [ "podman-caddy.service" "homepage-dashboard" ];
+    systemdWantedBy = [ "podman-caddy" ];
     # Specify the secrets you need
     secrets = {
       # The 1Password Secret Reference in here (the `op://` URI)
@@ -18,13 +18,13 @@
         CLOUDFLARE_API_KEY=op://Docker secrets/CF-api/Section_zt7q7yv43v24wegeyewpaikbjy/api token
       '';
       # you can also specify the UNIX file owner, group, and mode
-      #caddy-secret.user = "SomeServiceUser";
-      #caddy-secret.group = "SomeServiceGroup";
-      #caddy-secret.mode = "0600";
+      caddy-secret.user = "root";
+      caddy-secret.group = "root";
+      caddy-secret.mode = "0600";
       # If you need to, you can even customize the path that the secret gets installed to
-      caddy-secret.path = "/run/secrets/container/caddy-secret";
+      #caddy-secret.path = "/run/secrets/container/caddy-secret";
       # You can also disable symlinking the secret into the installation destination
-      #caddy-secret.symlink = false;
+      caddy-secret.symlink = true;
     };
   };
 }
