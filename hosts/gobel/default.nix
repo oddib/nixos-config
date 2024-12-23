673616970d7b84963088cff1a2d1f428b67fa553
oddib <60390653+oddib@users.noreply.github.com>
2024-12-23 14:34:36 +0000

    Update default.nix

--- hosts/gobel/default.nix
+++ hosts/gobel/default.nix
@@ -14,7 +14,7 @@
 
   # Bootloader.
   # boot.loader.grub.enable = true;
-  # boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.grub.device = "/dev/nvme0n1";
   # boot.loader.grub.useOSProber = true;
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
