167074fbf5bec572570efd85284314c678775311
Oddbjørn Rønnestad <oddbjorn.r@gmail.com>
2024-12-26 21:38:18 +0100

    update ports

--- common/containers/auth.nix
+++ common/containers/auth.nix
@@ -2,7 +2,6 @@
   virtualisation.oci-containers.containers."wizarr" = {
     image = "ghcr.io/wizarrrr/wizarr:latest";
     volumes = [ "/var/lib/container//wizarr:/data/database:rw" ];
-    ports = [ "5000:5000" ];
-
+    ports = [ "5000:5000" "5690:5690" ];
   };
 }
