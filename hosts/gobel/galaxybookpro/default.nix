9c914628bcfcef3e661fb19c068f02e8a087581b
Oddbjørn Rønnestad <oddbjorn.r@gmail.com>
2025-01-29 20:19:15 +0100

    trying  to fix monitor backlight

--- hosts/gobel/galaxybookpro/default.nix
+++ hosts/gobel/galaxybookpro/default.nix
@@ -1,7 +1,7 @@
 { ... }: {
   imports = [ ./samsung-galaxybook-extras ];
 
-  services.fprintd.enable = true;  
+  services.fprintd.enable = true;
   hardware.samsung-galaxybook.enable = true;
-
+  boot.kernelParams = [ "i915.enable_dpcd_backlight=3" ];
 }
