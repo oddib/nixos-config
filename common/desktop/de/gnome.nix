84b423b7de771799fd51a0028123b4f70e31f4f0
Oddbjørn Rønnestad <oddbjorn.r@gmail.com>
2025-02-05 12:21:10 +0100

    disable gnome programs

--- common/desktop/de/gnome.nix
+++ common/desktop/de/gnome.nix
@@ -4,4 +4,6 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.core-utilities.enable = false;
+
 }
