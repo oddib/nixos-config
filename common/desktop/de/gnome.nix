22a3602ac00860f52a4aa0988897d1a7dba17b40
Oddbjørn Rønnestad <oddbjorn.r@gmail.com>
2025-02-11 23:19:46 +0100

    updated some stuff

--- common/desktop/de/gnome.nix
+++ common/desktop/de/gnome.nix
@@ -4,6 +4,6 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.gnome.core-utilities.enable = false;
+  #services.gnome.core-utilities.enable = false;
 
 }
--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "NixOS configuration";
 
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.follows = "nixos-cosmic/nixpkgs"; # NOTE: change "nixpkgs" to "nixpkgs-stable" to use stable NixOS release
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs.follows = "nixos-cosmic/nixpkgs"; # NOTE: change "nixpkgs" to "nixpkgs-stable" to use stable NixOS release
     nixos-cosmic = {
       url = "github:lilyinstarlight/nixos-cosmic";
-      #inputs.nixpkgs.follows = "nixpkgs";
-      #inputs.nixpkgs-stable.follows = "nixpkgs"; # Bad but i want faster eval time
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs"; # Bad but i want faster eval time
     };
     nix-flatpak = {
       url = "github:gmodena/nix-flatpak/?ref=latest";
