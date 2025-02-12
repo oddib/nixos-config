8ceda2b5abe5e891030b31adb6b8f69965abe637
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-02-12 14:31:26 +0100

    cleanup

--- common/desktop/flatpak.nix
+++
@@ -1,6 +0,0 @@
-{ inputs, ... }: {
-  imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
-  services.flatpak.enable = true;
-  services.flatpak.packages =
-    [ "com.adobe.Reader" "com.discordapp.Discord" "org.geogebra.GeoGebra" ];
-}
