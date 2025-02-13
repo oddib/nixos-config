78d6039855204c2bc79a7512ca25e8ab421a146a
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-02-13 11:50:49 +0100

    added git oauth config

--- home/git.nix
+++ home/git.nix
@@ -1,17 +1,22 @@
-{pkgs,lib,...}:
-{
-    programs.git = {
-    enable = true;
-    userName = "Oddbjørn Rønnestad";
-    userEmail = "60390653+oddib@users.noreply.github.com";
-    extraConfig = {
-      gpg = { format = "ssh"; };
-      "gpg \"ssh\"" = {
-        program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
-      };
-      commit = { gpgsign = true; };
+{ pkgs, lib, ... }: {
+  programs = {
+    git-credential-oauth.enable = true;
+    git = {
+      enable = true;
+      userName = "Oddbjørn Rønnestad";
+      userEmail = "60390653+oddib@users.noreply.github.com";
+      extraConfig = {
+        gpg = { format = "ssh"; };
+        "gpg \"ssh\"" = {
+          program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+        };
+        commit = { gpgsign = true; };
 
-      user = { signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJDCJ0s4kA8stxlBhrxhyN1bQyBh8LFE+HsoNZbas83V"; };
+        user = {
+          signingKey =
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJDCJ0s4kA8stxlBhrxhyN1bQyBh8LFE+HsoNZbas83V";
+        };
+      };
     };
   };
 }
