7e5a6f5433a0007b81f7355bd2df33fbc7543b78
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-02-25 18:17:07 +0100

    some cleanup

--- hosts/gobel/galaxybookpro/default.nix
+++
@@ -1,7 +0,0 @@
-{ ... }: {
-  imports = [ ./samsung-galaxybook-extras ];
-
-  services.fprintd.enable = true;
-  hardware.samsung-galaxybook.enable = true;
-  boot.kernelParams = [ "i915.enable_dpcd_backlight=3" ];
-}
