68e51620260a5c32d24cfafd96d3185e086ac48e
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-03-20 10:50:08 +0100

    fixed Hw-accelleration

--- home/apps/vivaldi.nix
+++ home/apps/vivaldi.nix
@@ -10,7 +10,7 @@
         proprietaryCodecs = true;
         enableWidevine = true;
         #vulkanSupport = true;
-        commandLineArgs = "--enable-features=AcceleratedVideoDecodeLinuxGL,VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE,UseOzonePlatform --ozone-platform=wayland";
+        commandLineArgs = "--enable-features=AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder,VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE";
       })
     ];
   };
