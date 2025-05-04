cd9bbac2a152c77092b55ca8d01fde7bfca9799c
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-05-04 10:23:39 +0200

    make cache server not use itself

--- common/cache/caches/edwin.nix
+++ common/cache/caches/edwin.nix
@@ -1,10 +1,16 @@
 {
-  nix = {
-    settings = {
-      substituters = ["https://nixcache.scuffedflix.no"];
-      trusted-public-keys = [
-        "nixcache.scuffedflix.no:GtnH8sPYpKYcIaQSfc92UD02uG9OG//h2qOuBPgoI1w="
-      ];
+  config,
+  lib,
+  ...
+}: {
+  config = lib.mkIf (!config.services.nix-serve.enable) {
+    nix = {
+      settings = {
+        substituters = ["https://nixcache.scuffedflix.no"];
+        trusted-public-keys = [
+          "nixcache.scuffedflix.no:GtnH8sPYpKYcIaQSfc92UD02uG9OG//h2qOuBPgoI1w="
+        ];
+      };
     };
   };
 }
