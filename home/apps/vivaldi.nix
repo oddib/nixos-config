c96b36f7ba54d48f1a07d35fabccc87717b799ac
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-07-27 00:14:16 +0200

    Fix: vivaldi overrides breaks

--- home/apps/vivaldi.nix
+++ home/apps/vivaldi.nix
@@ -6,14 +6,8 @@
 }: {
   config = lib.mkIf osConfig.system.desktop.enable {
     home.packages = with pkgs; [
-      firefox ## Firefox for fallback
-      ## Vivaldi browser with proprietary codecs and widevine support
-      (vivaldi.override {
-        proprietaryCodecs = true;
-        enableWidevine = true;
-        #vulkanSupport = true;
-        commandLineArgs = "--enable-features=AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder,VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,ignore-gpu-blocklist,enable-unsafe-webgpu,enable-zero-copy";
-      })
+      ## Vivaldi 
+      vivaldi
     ];
   };
 }
