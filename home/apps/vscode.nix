1ad3e6e33aa4abc693ae5faef69a3e41b1c9accb
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-07-28 14:29:29 +0200

    refactor vscode default

--- home/apps/vscode.nix
+++ home/apps/vscode.nix
@@ -3,49 +3,65 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  sharedUserSettings = {
+    "extensions.autoUpdate" = true;
+    "files.autoSave" = "afterDelay";
+    "git.confirmSync" = false;
+    "git.enableSmartCommit" = true;
+    "git.autofetch" = true;
+  };
+  sharedExtensions =
+    with pkgs.vscode-extensions; [
+      tailscale.vscode-tailscale
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "op-vscode";
+        publisher = "1Password";
+        version = "1.0.5";
+        sha256 = "sha256-J7vAK2t6fSjm5i6y3+88aO84ipFwekQkJMD7W3EIWrc=";
+      }
+    ];
+in {
   config = lib.mkIf osConfig.system.desktop.enable {
     programs.vscode = {
       enable = true;
-      profiles.default = {
-        #/home/oddbjornmr/nixos/flake.nix
-        userSettings = {
-          "extensions.autoUpdate" = true;
-          "nix.enableLanguageServer" = true;
-          "nix.serverPath" = "nixd";
-          "git.confirmSync" = false;
-          "git.enableSmartCommit" = true;
-          "files.autoSave" = "afterDelay";
-          "git.autofetch" = true;
-          "nix.serverSettings" = {
-            "nixd" = {
-              "formatting" = {
-                "command" = ["alejandra"];
-              };
-              "options" = {
-                "nixos" = {
-                  "expr" = "(builtins.getFlake \"/home/oddbjornmr/nixos-config/flake.nix\").nixosConfigurations.edwin.options";
+      profiles = {
+        default = {
+          userSettings = sharedUserSettings;
+          extensions = sharedExtensions;
+        };
+
+        nix = {
+          userSettings = sharedUserSettings // {
+            "nix.enableLanguageServer" = true;
+            "nix.serverPath" = "nixd";
+            "nix.serverSettings" = {
+              "nixd" = {
+                "formatting" = {
+                  "command" = ["alejandra"];
+                };
+                "options" = {
+                  "nixos" = {
+                    "expr" = "(builtins.getFlake \"/home/oddbjornmr/nixos-config/flake.nix\").nixosConfigurations.edwin.options";
+                  };
                 };
               };
             };
           };
-        };
-        extensions = with pkgs.vscode-extensions;
-          [
+          extensions = sharedExtensions ++ (with pkgs.vscode-extensions; [
             jnoortheen.nix-ide
-            # ms-python.python
-            # ms-azuretools.vscode-docker
-            # ms-vscode-remote.remote-ssh
-            tailscale.vscode-tailscale
-          ]
-          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-            {
-              name = "op-vscode";
-              publisher = "1Password";
-              version = "1.0.5";
-              sha256 = "sha256-J7vAK2t6fSjm5i6y3+88aO84ipFwekQkJMD7W3EIWrc=";
-            }
-          ];
+          ]);
+        };
+
+        python-dev = {
+          userSettings = sharedUserSettings // {
+            # Add any Python-specific settings here
+          };
+          extensions = sharedExtensions ++ (with pkgs.vscode-extensions; [
+            ms-python.python
+          ]);
+        };
       };
     };
   };
