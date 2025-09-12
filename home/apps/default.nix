86b912b83761d1dec4d79bf8bf96588e598bc82b
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-09-12 13:00:12 +0200

    refactor

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -1,3 +1,17 @@
-{...}: {
-  imports = [./vivaldi.nix ./vscode.nix  ./games.nix ./flatpak.nix];
+{
+  osConfig,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [./vscode.nix ./games.nix ./flatpak.nix];
+  config = lib.mkIf osConfig.system.desktop.enable {
+    home.packages = with pkgs; [
+      ## Vivaldi
+      vivaldi
+    ];
+    programs = {
+      vscode.enable = lib.mkDefault true;
+    };
+  };
 }
--- home/shell/default.nix
+++ home/shell/default.nix
@@ -1,12 +1,48 @@
-{pkgs, ...}: {
-  # config for shell apps
-  imports = [
-    ./git.nix
-    ./ssh.nix
-    ./sh.nix
-    ./starship.nix
-  ];
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib) mkDefault;
+in {
+  programs = {
+    bash = {
+      enable = mkDefault true;
+      enableCompletion = true;
+      # TODO add your custom bashrc here
+      bashrcExtra = ''
+        export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+      '';
+    };
+    starship = {
+      enable = mkDefault true;
+    };
 
+    git = {
+      enable = mkDefault true;
+      userName = "Oddbjørn Rønnestad";
+      userEmail = "60390653+oddib@users.noreply.github.com";
+      extraConfig = {
+        gpg.format = "ssh";
+        "gpg \"ssh\"".program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+        commit.gpgsign = true;
+        user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJDCJ0s4kA8stxlBhrxhyN1bQyBh8LFE+HsoNZbas83V";
+      };
+    };
+    ssh = {
+      enable = mkDefault true;
+      enableDefaultConfig = false;
+      matchBlocks = {
+        "*" = {
+          identityAgent = "~/.1password/agent.sock";
+        };
+        "scuffedflix" = {
+          user = "oddbjornmr";
+          forwardAgent = true;
+        };
+      };
+    };
+  };
   home.packages = with pkgs; [
     fastfetch
 
