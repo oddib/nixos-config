8a015768488287de04e556c3ff6347b17140c5b9
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-09-26 13:09:13 +0200

    remove newline

--- common/apps/shell/starship/default.nix
+++ common/apps/shell/starship/default.nix
@@ -1,7 +1,11 @@
 {pkgs, ...}: {
   fonts.packages = [pkgs.nerd-fonts.fira-code];
+  environment.systemPackages = [pkgs.starship];
   programs.starship = {
-    enable = true; 
-    presets = ["nerd-font-symbols"];
+    enable = true;
+    settings = {
+      add_newline = false;
+    };
+    presets = ["gruvbox-rainbow"];
   };
 }
