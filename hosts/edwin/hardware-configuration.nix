b233c72e5007fffc269a99adad48b85a0d2ed8ef
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-10-01 13:18:29 +0200

    remove kernel modules as they are not needed

--- hosts/edwin/hardware-configuration.nix
+++ hosts/edwin/hardware-configuration.nix
@@ -11,8 +11,6 @@
     (modulesPath + "/installer/scan/not-detected.nix")
     inputs.nixos-hardware.nixosModules.common-gpu-amd
     inputs.nixos-hardware.nixosModules.common-cpu-amd
-    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-    inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
     inputs.nixos-hardware.nixosModules.common-pc-ssd
   ];
   boot = {
