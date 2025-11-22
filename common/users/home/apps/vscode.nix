3bf2ef244a4fb2709d590b9a893b6277d0c0597a
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-11-22 16:55:22 +0100

    added rust profile to vscode

--- common/users/home/apps/vscode.nix
+++ common/users/home/apps/vscode.nix
@@ -10,22 +10,27 @@
     "git.enableSmartCommit" = true;
     "git.autofetch" = true;
   };
-  sharedExtensions =
-    with pkgs.vscode-extensions; [
-      tailscale.vscode-tailscale
+  sharedExtensions = with pkgs.vscode-extensions;
+    [
       jnoortheen.nix-ide
+      mkhl.direnv
     ]
-    /*
-       ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
       {
+        name = "vscode-sqlite";
+        publisher = "alexcvzz";
+        version = "0.14.1";
+        sha256 = "sha256-jOQkRgBkUwJupD+cRo/KRahFRs82X3K49DySw6GlU8U=";
+      }
+      /*
+         {
         name = "op-vscode";
         publisher = "1Password";
         version = "1.0.5";
         sha256 = "sha256-J7vAK2t6fSjm5i6y3+88aO84ipFwekQkJMD7W3EIWrc=";
       }
-    ]
-    */
-    ;
+      */
+    ];
 in {
   programs.vscode = {
     enable = osConfig.roles.desktop.enable;
@@ -58,29 +63,31 @@ in {
           sharedExtensions;
       };
 
-      python-dev = {
+      rust-dev = {
         userSettings =
           sharedUserSettings
           // {
-            # Add any Python-specific settings here
           };
         extensions =
           sharedExtensions
           ++ (
-            with pkgs.vscode-extensions;
-              [
-                ms-python.python
-                ms-toolsai.jupyter
-                mkhl.direnv
-              ]
-              ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-                {
-                  name = "vscode-sqlite";
-                  publisher = "alexcvzz";
-                  version = "0.14.1";
-                  sha256 = "sha256-jOQkRgBkUwJupD+cRo/KRahFRs82X3K49DySw6GlU8U=";
-                }
-              ]
+            with pkgs.vscode-extensions; [
+              rust-lang.rust-analyzer
+            ]
+          );
+      };
+      python-dev = {
+        userSettings =
+          sharedUserSettings
+          // {
+          };
+        extensions =
+          sharedExtensions
+          ++ (
+            with pkgs.vscode-extensions; [
+              ms-python.python
+              ms-toolsai.jupyter
+            ]
           );
       };
     };
