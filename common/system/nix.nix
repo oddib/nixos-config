4eda98c1575f88c3b74e6e32db774828d6d45d8e
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-12-11 01:29:24 +0100

    update flake lock and add devenv substituters

--- common/system/nix.nix
+++ common/system/nix.nix
@@ -1,10 +1,16 @@
 {
-  nix.settings = {
-    auto-optimise-store = true;
-    experimental-features = ["nix-command" "flakes"];
-    #substituters = ["https://cache.nixos.org/" "https://nixcache.scuffedflix.no"];
-    #trusted-public-keys = [
-    #  "nixcache.scuffedflix.no:GtnH8sPYpKYcIaQSfc92UD02uG9OG//h2qOuBPgoI1w="
-    #];
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = ["nix-command" "flakes"];
+      #substituters = ["https://cache.nixos.org/" "https://nixcache.scuffedflix.no"];
+      #trusted-public-keys = [
+      #  "nixcache.scuffedflix.no:GtnH8sPYpKYcIaQSfc92UD02uG9OG//h2qOuBPgoI1w="
+      #];
+    };
+    extraOptions = ''
+      extra-substituters = https://devenv.cachix.org
+      extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+    '';
   };
 }
