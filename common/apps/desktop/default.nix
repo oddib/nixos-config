8b8ce5f3cc2ff6c8914b1b1c09df2bc13d0e79d4
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-12-15 20:21:23 +0100

    nixos flake udate and disable orca and gnome with stable cosmic

--- common/apps/desktop/default.nix
+++ common/apps/desktop/default.nix
@@ -12,21 +12,15 @@
     services.desktopManager.cosmic.enable = true;
     services.displayManager.cosmic-greeter.enable = true;
     services.displayManager.defaultSession = "cosmic";
-    # Try to fix flickering with vrr on cosmic
-    environment.etc."profile.d".text = "COSMIC_DISABLE_DIRECT_SCANOUT=1";
 
-    # xdg defaults not working.
-    # https://github.com/lilyinstarlight/nixos-cosmic/issues/273
-    systemd.user.extraConfig = ''
-      DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
-    '';
+    services.orca.enable = lib.mkForce false; ## SOOOOOO ANNOYING
 
     ###########################################################################
 
     ##  Enable the Gnome Desktop Environment.
 
-    services.displayManager.gdm.enable = true;
-    services.desktopManager.gnome.enable = true;
+    #services.displayManager.gdm.enable = true;
+    #services.desktopManager.gnome.enable = true;
     # gnome.core-utilities.enable = false;
 
     ###########################################################################
