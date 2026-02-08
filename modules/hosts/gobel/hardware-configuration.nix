4efb03eb02208358611bb999b3b4dab3145f264a
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2026-02-08 14:45:25 +0100

    move gobel to facter config

--- modules/hosts/gobel/hardware-configuration.nix
+++ modules/hosts/gobel/hardware-configuration.nix
@@ -1,8 +1,5 @@
-{inputs, ...}: {
-  flake.modules.nixos.gobel = {modulesPath, ...}: {
-    imports = [
-      (modulesPath + "/installer/scan/not-detected.nix")
-      inputs.nixos-hardware.nixosModules.samsung-galaxybook-2-pro
-    ];
+{...}: {
+  flake.modules.nixos.gobel = {...}: {
+    hardware.facter.reportPath = ./facter.json;
   };
 }
--- modules/nix/tools/nixos-hardware/flake-parts.nix
+++
@@ -1,8 +0,0 @@
-{ ...}: {
-  # Manage a user environment using Nix
-  # https://github.com/nix-community/home-manager
-
-  flake-file.inputs = {
-    nixos-hardware.url = "github:oddib/nixos-hardware";
-  };
-}
