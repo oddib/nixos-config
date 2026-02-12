9b1f6c03d229b1f671adb668031fad6096e3208e
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2026-02-12 14:38:19 +0100

    hardware config for gobel

--- modules/hosts/gobel/hardware-configuration.nix
+++ modules/hosts/gobel/hardware-configuration.nix
@@ -1,5 +1,7 @@
 {...}: {
   flake.modules.nixos.gobel = {...}: {
     hardware.facter.reportPath = ./facter.json;
+    hardware.facter.detected.networking=false;
+    boot.kernelParams = ["i915.enable_dpcd_backlight=3"];
   };
 }
