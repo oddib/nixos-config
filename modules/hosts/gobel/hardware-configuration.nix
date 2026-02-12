22ca455793fe7f7c6a56efc783844dcc9b9c5be8
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2026-02-12 15:03:18 +0100

    small fixes

--- modules/hosts/gobel/hardware-configuration.nix
+++ modules/hosts/gobel/hardware-configuration.nix
@@ -1,7 +1,7 @@
-{...}: {
+{lib,...}: {
   flake.modules.nixos.gobel = {...}: {
     hardware.facter.reportPath = ./facter.json;
-    hardware.facter.detected.networking=false;
+    hardware.facter.detected.dhcp.enable = lib.mkForce false;
     boot.kernelParams = ["i915.enable_dpcd_backlight=3"];
   };
 }
