709b041aff757fd2d9b4dd9569e6c8982f3f002d
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2026-06-09 14:08:09 +0200

    Update caddy

--- modules/services/server/caddy/caddy.nix
+++ modules/services/server/caddy/caddy.nix
@@ -7,7 +7,7 @@
         plugins = [
           "github.com/caddy-dns/cloudflare@v0.2.2"
         ];
-        hash = "sha256-dNmccsad8nFm2X9w/6mdgAdhPSquNm6oVhf3sNc46SQ=";
+        hash = "sha256-wHW0l15aLswe7gV9WioXo//abd0sJI82I7zIroRG3uU=";
       };
       globalConfig = ''
         acme_dns cloudflare {$CLOUDFLARE_API_KEY}
