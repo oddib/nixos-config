a4f9824c925a06f3d5897d0a4d4b91e8548f8337
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2026-06-17 13:31:08 +0200

    Remove gnome

--- modules/programs/desktop/desktop.nix
+++ modules/programs/desktop/desktop.nix
@@ -24,8 +24,8 @@
 
     ##  Enable the Gnome Desktop Environment.
 
-    #services.displayManager.gdm.enable = true;
-    services.desktopManager.gnome.enable = true;
+    # services.displayManager.gdm.enable = true;
+    # services.desktopManager.gnome.enable = true;
     # gnome.core-utilities.enable = false;
 
     ###########################################################################
